b2d3c86d924fc4b70caa6f082dbaf895755ca358
Yogesh12P <patil1yogesh21@gmail.com>
2017-09-23 09:28:57 +0530

    Drag and Drope
    dynamic table

---
+++ dynamicTable/dynamicTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dynamicTable
+//
+//  Created by BridgeLabz on 21/09/17.
+//  Copyright © 2017 Self Emp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
